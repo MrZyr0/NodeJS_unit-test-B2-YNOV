320b3626600471a8cced3597273e2106156bb7af
Julien <julien.seixas@free.fr>
2019-05-16 15:44:05 +0200

    fix tests

--- test/api.js
+++ test/api.js
@@ -5,6 +5,16 @@ const expect = require('chai').expect
 
 
 describe('Get user', function (){
-    it('username return for a valid user', function(){ expect(api.getUser('Leanne Graham')).to.be.eql('200 User Found') })
-    it('username return for a wrong user', function(){ expect(api.getUser('wxy')).to.be.eql('404 User Not Found') })
+    it('return type', async function(){ 
+        let returned = await api.apiCallUser('');
+        expect(returned).to.be.a('string') 
+    })
+    it('username return for a valid user', async function(){
+        let returned = await api.apiCallUser('Leanne Graham')
+        expect(returned).to.be.eql('200 User Found')
+    })
+    it('username return for a wrong user', async function(){
+        let returned = await api.apiCallUser('wxy')
+        expect(returned).to.be.eql('404 User Not Found')
+    })
 })
